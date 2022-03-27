4f2344dbdcfa6856c7114276418bbe8553346e1c
devopslondon2021 <devopslondon2019@gmail.com>
2022-03-27 16:42:11 +0100

    Change schema

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,16 +1,16 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { Contract, Approval, Transfer } from "../generated/Contract/Contract"
-import { Transaction } from "../generated/schema"
+import { ExampleEntity } from "../generated/schema"
 
 export function handleApproval(event: Approval): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = Transaction.load(event.transaction.from.toHex())
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new Transaction(event.transaction.from.toHex())
+    entity = new ExampleEntity(event.transaction.from.toHex())
 
     // Entity fields can be set using simple assignments
     entity.count = BigInt.fromI32(0)
